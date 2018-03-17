5713fc534873f3ddbb007c94998c6d8f8820e118
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-17 14:18:17 -0300

    Repositories's page adjustments

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,6 +1,31 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+  AsyncStorage,
+} from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import Proptypes from 'prop-types';
 
-const Repositories = () => <View />;
+import api from 'services/api';
+
+class Repositories extends Component {
+    static navigationOptions = {
+      header: null,
+    };
+
+    state = {
+      data: [],
+      loading: true,
+      refreshing: false,
+    };
+
+    render() {
+      return <View />;
+    }
+}
 
 export default Repositories;
